c6bd217a97ec71f191dcbcd89294edaf74932e25
Richard Scheglmann <r.scheglmann@gmail.com>
2021-06-03 12:41:40 +0200

    Erste Version erweitert mit Bulma CSS und SASS loader.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 
-console.log('Hello World');
+import "bulma";
+import "./style/myStyle.sass";
+
+//start
+const app = document.getElementById("app");
+
+const p = document.createElement("p");
+p.textContent = "Hello World";
+
+const button = document.createElement("button");
+button.textContent = "Drück mich!";
+button.className = "button is-primary";
+
+const a = document.createElement("a");
+a.href = "https://bulma.io/documentation/overview/modifiers/";
+a.target = "_blank";
+a.textContent = "bulma CSS Buttons";
+
+app?.append(p);
+app?.append(button);
+app?.append(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,53 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+
+  mode: 'development', // development oder production
+
   entry: './src/index.ts',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  devServer: {
+    contentBase: 'dist',
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html', to: '' },
+      ],
+    }),
+  ],
+
   module: {
     rules: [
       {
         test: /\.ts$/,
         include: [path.resolve(__dirname, 'src')],
         use: 'ts-loader',
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ]
   },
-  output: {
-    
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
 
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-  },
 };
